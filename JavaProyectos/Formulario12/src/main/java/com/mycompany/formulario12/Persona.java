d755c99a50c9410ebf8be979b41d1e1ab96126de
damian <damianmelendez5678@gmail.com>
2025-09-19 20:13:55 -0600

    actualización Del dia

---
+++ JavaProyectos/Formulario12/src/main/java/com/mycompany/formulario12/Persona.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.formulario12;
+
+/**
+ *
+ * @author DELL
+ */
+public abstract class Persona {
+    private String Nombre;
+    private String Apellido;
+    private boolean Genero;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public boolean isGenero() {
+        return Genero;
+    }
+
+    public void setGenero(boolean Genero) {
+        this.Genero = Genero;
+    }
+
+    public Persona() {
+    }
+
+    public Persona(String Nombre, String Apellido, boolean Genero) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Genero = Genero;
+    }
+    
+    public abstract String darNombreCompleto();
+    public abstract boolean darGenero();
+}
