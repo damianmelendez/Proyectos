c34f7e10d0f92892aa09217f99d589a4bba57d08
damian <damianmelendez5678@gmail.com>
2025-09-19 19:33:24 -0600

    actualización Del dia

---
+++ JavaProyectos/Formulario10/src/main/java/com/mycompany/formulario10/Formulario10.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulario10;
+
+/**
+ *
+ * @author DELL
+ */
+public class Formulario10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JClientes J = new JClientes();
+        J.setVisible(true);
+    }
+}
---
+++ JavaProyectos/Formulario11/src/main/java/com/mycompany/formulario11/Formulario11.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulario11;
+
+/**
+ *
+ * @author DELL
+ */
+public class Formulario11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JCliente J = new JCliente();
+        J.setVisible(true);
+    }
+}
