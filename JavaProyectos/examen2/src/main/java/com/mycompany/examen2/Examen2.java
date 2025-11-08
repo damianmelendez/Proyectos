e5f389f80e66a675ae9c2f95e9ec3249254d0903
damian <damianmelendez5678@gmail.com>
2025-11-07 19:32:18 -0600

    actualización Del dia

---
+++ JavaProyectos/examen2/src/main/java/com/mycompany/examen2/Examen2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen2;
+
+/**
+ *
+ * @author DELL
+ */
+public class Examen2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
